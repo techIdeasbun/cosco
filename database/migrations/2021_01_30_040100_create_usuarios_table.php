e3aadf77129b2680240802488e7c77a5a75e3605
techIdeasbun <techideas.bun@gmail.com>
2021-03-07 10:50:12 -0500

    se instala admin lte y controles de acceso

--- database/migrations/2021_01_30_040100_create_usuarios_table.php
+++ database/migrations/2021_01_30_040100_create_usuarios_table.php
@@ -21,10 +21,8 @@ class CreateUsuariosTable extends Migration
             $table->string('identificacio');
 
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('role_id');
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
 
             $table->timestamps();
         });
---
+++ routes/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\HomeController;
+
+/* Route::get('', function(){
+    return "Hola administrador";
+}); */
+
+Route::get('', [HomeController::class, 'index']);
